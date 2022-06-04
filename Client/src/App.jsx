aafdcf45fe3201f680294d15a4461c691a2393a5
Aniket Mhaske <trainee3@smartshiphub.com>
2022-06-04 10:16:57 +0530

    Express-Server-Created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-=======
-import Siderbar from './Layout/Dashborad/Sidebar';
 import Login from './Auth/Login'
 import Register from './Auth/RegisterUser'
->>>>>>> d5a95d61c245e9c3a3710ad2d339173b29873382
 import './App.css'
 import Dashboard from './Layout/Dashboard/Dashboard';
 
@@ -12,13 +8,9 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
       <Dashboard />
-=======
-      <Siderbar />
       <Login/>
       <Register/>
->>>>>>> d5a95d61c245e9c3a3710ad2d339173b29873382
     </div>
   );
 }
